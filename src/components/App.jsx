7171e2691edb89c28f7a4e3df769c2d1b56006c6
Victor <victorkongym@gmail.com>
2022-11-25 15:13:49 +0000

    basic todo template

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function App() {
+    return (
+    <div>
+         <h1>TODO</h1>
+         <form>
+            <input name="todo-input" placeholder="Add TODO here"></input>
+            <button>Add Item</button>
+            <p>0 left to do</p>
+         </form>
+    </div>)
+};
+
+
+
+export default App;
