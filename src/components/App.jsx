bedb77a014096abb2d421d4aa11caa063b16a6e8
Victor <victorkongym@gmail.com>
2022-12-14 14:03:15 +0000

    modularise title

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Form from "./Form";
+import Title from "./Title";
 
 function App() {
   return (
     <div>
-      <h1>TODO</h1>
+      <Title />
       <Form />
     </div>
   );
