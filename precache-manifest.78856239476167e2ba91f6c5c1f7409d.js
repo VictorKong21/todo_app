fc4a6f2b5c34ca2b0fb41bf3ce21102f94c78dde
Victor <victorkongym@gmail.com>
2022-12-15 10:18:57 +0000

    Updates

---
+++ precache-manifest.78856239476167e2ba91f6c5c1f7409d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae0e2a8cda375807ee0b",
+    "url": "/todo_app/static/js/main.ae0e2a8c.chunk.js"
+  },
+  {
+    "revision": "19a6542aaed79e759838",
+    "url": "/todo_app/static/js/1.19a6542a.chunk.js"
+  },
+  {
+    "revision": "9881e67fb72b4b419ec0",
+    "url": "/todo_app/static/js/runtime~main.9881e67f.js"
+  },
+  {
+    "revision": "67b58f1e77b46fc5c485cb6c3f025d2b",
+    "url": "/todo_app/index.html"
+  }
+];
